68c0690cf027ec0c58cbf428842f9992bb23d5c2
OttGod12 <130059670+OttGod12@users.noreply.github.com>
2024-09-18 11:32:40 -0300

    certo

--- src/components/Pagina.jsx
+++ src/components/Pagina.jsx
@@ -1,8 +1,8 @@
 export function pagina(props){
 
     return(
-        <div>
-            <header>
+        <div classname= "w-full h-full bg-slate-200">
+            <header classname="bg-zinc">
                 <h1>{props.titulo}</h1>
                 <h2>{props.subtitulo}</h2>
             </header>
